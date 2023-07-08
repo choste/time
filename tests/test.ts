c4369b836a2acc5d6bb47f888a7100a89097d5d1
Carl <carl.hostetler@gmail.com>
2023-07-07 17:49:24 -0700

    Create node.js.yml

--- tests/test.ts
+++ tests/test.ts
@@ -6,6 +6,6 @@ test('index page has expected h1', async ({ page }) => {
 });
 
 test('tags page exist', async ({ page }) => {
-	await page.goto('/tags')
+	await page.goto('/tags');
 	await expect(page.getByText('Tags')).toBeVisible();
-})
+});
