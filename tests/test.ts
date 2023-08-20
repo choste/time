94a94058b2129f46619478ae796337f5443f9153
Carl Hostetler <carl.hostetler@gmail.com>
2023-08-20 09:21:27 -0700

    Replace index page to test deploy

--- tests/test.ts
+++ tests/test.ts
@@ -1,10 +1,5 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
-});
-
 test('tags page exist', async ({ page }) => {
 	await page.goto('/tags');
 	await expect(page.getByText('Tags')).toBeVisible();
